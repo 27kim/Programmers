4c5a791b75cb88eafe27d806c46f092c7d4020f2
27 Kim <doohyeonkim@gmail.com>
2019-03-10 23:23:22 +0900

    File_IO 스터디 중

--- src/File_final.java
+++ src/File_final.java
@@ -9,9 +9,9 @@ import java.util.Arrays;
  *  Piped Input/OutPut Stream
  *  Audio Input/OutPut Stream
  * 1-1 바이트 보조 스트림
- *  Filter Input/OutPut Stream : 필터를 이용한 입출력 처리
- *  Buffered Input/OutPut Stream : 버퍼를 이용한 입출력 성능 향상
- *  Data Input/OutPut Stream : 기본형 단위로 데이터를 처리하는 기능
+ *  Filter Input/OutPut Stream : 모든 보조스티림의 조상
+ *      Buffered Input/OutPut Stream : 버퍼를 이용한 입출력 성능 향상
+ *      Data Input/OutPut Stream : 기본형 단위로 데이터를 처리하는 기능
  * 2. 문자 기반 스트림 : 문자 입출력 할 때 사용함
  *  File Reader/Writer
  *  CharArray Reader/Writer
@@ -78,6 +78,7 @@ public class File_final {
 
         /**
          * BufferedOutputStream
+         *  : 버퍼를 통한 성능 향상
          * */
 
         FileOutputStream fileOutputStream = new FileOutputStream("1234.txt");
@@ -91,5 +92,49 @@ public class File_final {
         //fos로 close 하면 bufferedOutputStream 에 남아있는 것들은 출력이 안됨
 //        fos.close();
         bufferedOutputStream.close();
+
+        /**
+         * DataInputStream / DataOutputStream
+         *  : 데이터를 읽고 쓰는데 있어서 byte 단위가 아닌 8가지 기본 자료형의 단위로 읽고 쓸 수 있다는 점이 있다.
+         *  : 출력한 데이터를 다시 읽어 올 때는 출력했을 때의 순서를 염두에 두어야 한다.
+         * */
+
+        FileOutputStream fileOutputStream1 = new FileOutputStream("sample2.dat");
+        DataOutputStream dos = new DataOutputStream(fileOutputStream1);
+
+
+        int [] scores = {100,90,80,50,40,70,88};
+
+        for(int i : scores){
+            dos.writeInt(i);
+        }
+
+        dos.close();
+
+        FileInputStream fileInputStream = new FileInputStream("sample2.dat");
+        DataInputStream dis = new DataInputStream(fileInputStream);
+
+        /**
+         * 데이터를 읽는 메서드 readInt 등은 더 이상 읽을 데이터가 없을 경우
+         * EOFException 을 발생시킨다.
+         *
+         * finally 에서 DataInputStream을 닫도록 처리 해야 한다
+         * */
+        int sum = 0;
+        try {
+            while (true) {
+                int input = dis.readInt();
+                System.out.println(input);
+                sum += input;
+            }
+        }catch (EOFException e ){
+            System.out.println("총합은 " + sum + "입니다");
+        }finally {
+            try{
+                if(dis!=null) dis.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
